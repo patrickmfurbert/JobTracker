4c1ebe421f131cf592877171653c5c95e914959b
Patrick Furbert <patrickmfurbert@gmail.com>
2021-10-06 15:02:29 +0000

    setup boilerplate for backend server

---
+++ back_end/app.js
@@ -0,0 +1,23 @@
+// Authors: Joey Punzel, Patrick Furbert
+
+//get .env information
+require('dotenv').config()
+
+// middleware
+require('dotenv').config()
+const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+//configure app
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+const port = 3000;
+
+// Start express app
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
