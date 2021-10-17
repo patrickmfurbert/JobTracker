c29cd44c3fb7765272e312048fb870c43f477fb7
Joey Punzel <joeypunzel@gmail.com>
2021-10-17 14:55:43 -0700

    initial setup (model/route/auth/createApp)

--- back_end/app.js
+++ back_end/app.js
@@ -8,12 +8,22 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const routes = require("./routes");
+
 const app = express();
+
+//database connection
+mongoose.connect("mongodb://localhost:27017/jobtracker", { useNewUrlParser: true })
+.then(() => console.log("Connected to mongoDB, database jobtracker"))
+.catch (console.error);
 
 //configure app
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+app.use("/", routes);
 
 const port = 5000;
 
