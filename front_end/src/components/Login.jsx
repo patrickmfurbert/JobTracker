3215d70b0783fd1938a2bfd5e9807e9de4d5b45b
Patrick Furbert <patrickmfurbert@gmail.com>
2021-10-20 13:04:32 +0000

    deleted boilerplate frontend code, updating Login.jsx and Login.css

---
+++ front_end/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import '../css/Login.css';
+
+
+export default function Login() {
+
+            return(
+                    <>
+                    </>
+            );
+}
