301cd9ee38182aab2a970686150f3d6237f9f05e
Patrick Furbert <patrickmfurbert@gmail.com>
2021-10-20 19:13:19 +0000

    updated Login.jsx

--- front_end/src/components/Login.jsx
+++ front_end/src/components/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import '../css/Login.css';
 import axios from 'axios';
+import tracker_icon from '../img/geo.svg';
 
 
 export default function Login() {
@@ -18,7 +19,16 @@ export default function Login() {
             return(
                     <>
                         <div id="login_container" class="card">
-                            <div className="card-body">
+                            <div className="card-body m-4">
+
+                                <div className="login_logo mb-3">
+                                    <img src={tracker_icon} alt="tracker" id="tracker"/>
+
+                                    <h1 className="display-6 mb-0 ms-2" id="jobtracker_h1">
+                                    Job Tracker
+                                    </h1>
+                                </div>
+
                                 <form onSubmit={handleSubmit(submit)}>
                                     <label htmlFor="username" className="form-label">username</label>
                                     <input type="text" name="username" id="username" className="form-control" />
@@ -28,6 +38,8 @@ export default function Login() {
 
                                     <input type="submit" value="Login" className="btn btn-dark mt-4 form-control"/>
                                 </form>
+
+                                <p className="mt-2">Don't have an account? <a href="#">Sign up</a></p>
                             </div>
                         </div>
                     </>
