7e7b8fa8cb87e246db1c841ab4c33ef550f06769
Patrick Furbert <patrickmfurbert@gmail.com>
2021-10-31 18:08:51 +0000

    updated nav component with appropriate selections

--- front_end/src/components/Custom_Nav.jsx
+++ front_end/src/components/Custom_Nav.jsx
@@ -10,6 +10,7 @@ export default function Custom_Nav(){
                 <>
                     <Navbar bg="light" expand={false}>
                         <Container fluid>
+
                             <Navbar.Brand href="#">Job Tracker</Navbar.Brand>
                             <Navbar.Toggle aria-controls="offcanvasNavbar" />
                             <Navbar.Offcanvas
@@ -17,21 +18,15 @@ export default function Custom_Nav(){
                             aria-labelledby="offcanvasNavbarLabel"
                             placement="end"
                             >
+
+
                             <Offcanvas.Header closeButton>
-                                <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
+                                <Offcanvas.Title id="offcanvasNavbarLabel">Navigation</Offcanvas.Title>
                             </Offcanvas.Header>
                             <Offcanvas.Body>
                                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                                <Nav.Link href="#action1">Home</Nav.Link>
-                                <Nav.Link href="#action2">Link</Nav.Link>
-                                <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-                                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action5">
-                                    Something else here
-                                    </NavDropdown.Item>
-                                </NavDropdown>
+                                <Nav.Link>Add Job Application</Nav.Link>
+                                <Nav.Link>Lookup Job Applications</Nav.Link>
                                 </Nav>
 
                             </Offcanvas.Body>
